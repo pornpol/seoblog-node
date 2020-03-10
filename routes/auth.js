f93a5160539f353eb0f38a78bb6c6268bedbc29d
pornpol <pornpol.w@gmail.com>
2020-03-10 20:26:44 +0700

    Add signout and protect routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { signup, signin } = require('../controllers/auth');
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin
+} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -12,5 +17,13 @@ const {
 
 router.post('/signup', userSignupValidator, runValidation, signup);
 router.post('/signin', userSigninValidator, runValidation, signin);
+router.get('/signout', signout);
+
+// test
+router.get('/secret', requireSignin, (req, res) => {
+  res.json({
+    message: 'You have access to secret page'
+  });
+});
 
 module.exports = router;
