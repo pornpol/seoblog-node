685c7cdf52244740a7e59fc22b2f636209183905
pornpol <pornpol.w@gmail.com>
2020-03-18 20:57:38 +0700

    List all blogs

--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const { create } = require('../controllers/blog');
+const {
+  create,
+  list,
+  listAllBlogsCategoriesTags,
+  read,
+  remove,
+  update
+} = require('../controllers/blog');
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
-const { read } = require('../controllers/user');
 
 const router = express.Router();
 
 router.post('/blog', requireSignin, adminMiddleware, create);
+router.get('/blogs', list);
+router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
+router.get('/blog/:slug', read);
+router.delete('/blog/:slug', requireSignin, adminMiddleware, remove);
+router.put('/blog/:slug', requireSignin, adminMiddleware, update);
 
 module.exports = router;
