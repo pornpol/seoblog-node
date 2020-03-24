c6cf0fb67893ac3be0774141c5b0ae1b4cde23d7
pornpol <pornpol.w@gmail.com>
2020-03-24 23:11:46 +0700

    Add user profile route and controller

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,13 @@ const {
   authMiddleware,
   adminMiddleware
 } = require('../controllers/auth');
-const { read } = require('../controllers/user');
+const { read, publicProfile, update, photo } = require('../controllers/user');
 
 const router = express.Router();
 
-router.get('/profile', requireSignin, authMiddleware, read);
+router.get('/user/profile', requireSignin, authMiddleware, read);
+router.get('/user/:username', publicProfile);
+router.put('/user/update', requireSignin, authMiddleware, update);
+router.get('/user/photo/:username', photo);
 
 module.exports = router;
